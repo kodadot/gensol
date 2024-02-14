4a3b3b40951bd075b117e777082d487967243053
Viki Val <viktorko99@gmail.com>
2024-02-14 15:27:28 +0100

    :zap: ERC-7572

--- src/Generative.sol
+++ src/Generative.sol
@@ -8,11 +8,31 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Generative is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
     uint256 private _nextTokenId;
+    string private _contractURI;
 
-    constructor(address initialOwner, string memory name_, string memory symbol_)
+    event ContractURIUpdated();
+
+    constructor(address initialOwner, string memory name_, string memory symbol_, string memory contractURI_)
         ERC721(name_, symbol_)
         Ownable(initialOwner)
-    {}
+    {
+        _contractURI = contractURI_;
+    }
+
+    // or e.g. "https://external-link-url.com/my-contract-metadata.json";
+
+    function contractURI() external view returns (string memory) {
+        return _contractURI;
+        // or e.g. for onchain:
+        // string memory json = '{"name": "Creatures","description":"..."}';
+        // return string.concat("data:application/json;utf8,", json);
+    }
+
+    /// @dev Suggested setter, not explicitly specified as part of this ERC
+    function setContractURI(string memory newURI) external onlyOwner {
+        _contractURI = newURI;
+        emit ContractURIUpdated();
+    }
 
     function safeMint(address to, string memory uri) public onlyOwner {
         uint256 tokenId = _nextTokenId++;
