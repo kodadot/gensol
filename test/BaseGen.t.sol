cbbf8a4e0a72ae4e67a8b591c3a3cc0dc1b911fa
Viki Val <viktorko99@gmail.com>
2024-05-16 13:20:46 +0200

    :test_tube: Basegen royalty + split

--- test/BaseGen.t.sol
+++ test/BaseGen.t.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.20;
 import "forge-std/Test.sol";
 import "../src/BaseGen.sol";
 
+// https://hackernoon.com/implementing-the-erc-2981-nft-royalty-standard-with-solidity
 contract BaseGenTest is Test {
     BaseGen public instance;
 
@@ -14,9 +15,11 @@ contract BaseGenTest is Test {
         string memory contractURI = "ipfs://";
         string memory baseURI = "https://data.kodadot.xyz/base/"; // suffixed with /?hash=
         uint256 maxSupply = 256;
-        instance = new BaseGen(initialOwner, name, symbol, contractURI, baseURI, maxSupply);
+        address receiver = vm.addr(2);
+        instance = new BaseGen(initialOwner, name, symbol, contractURI, baseURI, maxSupply, receiver);
         // instance.safeMint(initialOwner);
         instance.safeMint{value: 0.01 ether}(initialOwner);
+        // instance.setMaxSupply(128);
     }
 
     function testName() public view {
@@ -33,5 +36,17 @@ contract BaseGenTest is Test {
         string memory expected = string.concat(baseUri, scAddress, "/");
 
         assertEq(instance.tokenURI(0), string.concat(expected, "0"));
+    }
+
+    function testRoyalty() public view {
+        (address receiver, uint256 amount) = instance.royaltyInfo(0, 1 ether);
+        assertEq(receiver, vm.addr(2));
+        assertEq(amount, 0.05 ether);
+    }
+
+
+    function testValue() public view {
+        address receiver = vm.addr(2);
+        assertEq(receiver.balance, 0.01 ether);
     }
 }
