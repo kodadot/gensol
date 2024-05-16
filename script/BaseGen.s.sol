656cb305ea3ea78a8bb99bf64a3caab14fa7dd6f
Viki Val <viktorko99@gmail.com>
2024-05-16 15:14:10 +0200

    :technologist: Base Script

--- script/BaseGen.s.sol
+++ script/BaseGen.s.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.20;
 
 import "forge-std/Script.sol";
-// import "forge-std/console2.sol";
+import "forge-std/console2.sol";
 import "../src/BaseGen.sol";
 
 contract MyScript is Script {
@@ -12,14 +12,15 @@ contract MyScript is Script {
         vm.startBroadcast(deployerPrivateKey);
 
         address initialOwner = vm.addr(deployerPrivateKey);
-        string memory name = "Vortices";
-        string memory symbol = "VTX";
-        string memory contractURI = "ipfs://bafkreicthpidyo3gznp3uuezvweiur7xcbo5qcwgw3x4teui2psoqxxbgq";
-        string memory baseURI = "https://dyndata.deno.dev/base/content/"; // suffixed with /?hash=
-        uint256 maxSupply = 128;
+        string memory name = "Koda.";
+        string memory symbol = "KODA";
+        string memory contractURI = "ipfs://bafkreige4zmihk32by3n5aeoq7svlqeyhoaapt4l7hoyspsnr2hm7ljjgq";
+        string memory baseURI = "https://dyndata.deno.dev/base/content/"; 
+        uint256 maxSupply = 64;
+        address receiver = 0xE844b2a0a6453250c920BD2b4B7741946aB16C08;
 
-        BaseGen nft = new BaseGen(initialOwner, name, symbol, contractURI, baseURI, maxSupply);
-        // console.log("Contract deployed to %s", address(nft));
+        BaseGen nft = new BaseGen(initialOwner, name, symbol, contractURI, baseURI, maxSupply, receiver);
+        console2.log("Contract deployed to %s", address(nft));
 
         vm.stopBroadcast();
     }
